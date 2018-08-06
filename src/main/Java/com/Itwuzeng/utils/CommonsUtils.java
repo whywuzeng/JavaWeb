8eb4ffc6161f8ab247bad3572c10ebd888092016
whywuzeng <jiushiqiangone@sina.com>
2018-08-06 18:39:27 +0800

    2018-8-6公司

---
+++ src/main/Java/com/Itwuzeng/utils/CommonsUtils.java
@@ -0,0 +1,12 @@
+package com.Itwuzeng.utils;
+
+import java.util.UUID;
+
+public class CommonsUtils {
+
+	//生成uuid方法
+	public static String getUUID(){
+		return UUID.randomUUID().toString();
+	}
+	
+}
